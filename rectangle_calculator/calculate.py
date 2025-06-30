0c594a8da4db7007b47b4d98fe88f637e5eb1f70
Makaziwe0203 <ntsenyanamakaziwe@gmail.com>
2025-06-30 19:05:40 +0200

    Lesson4

---
+++ rectangle_calculator/calculate.py
@@ -0,0 +1,7 @@
+def area(length, width):
+    # Funtion to calculate the area
+    return length * width
+
+def perimeter(length, width):
+    # Funtion to calculate the perimeter
+    return 2 * (length + width)
